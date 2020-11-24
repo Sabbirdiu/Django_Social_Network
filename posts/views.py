790e40f7ed5a0af5cfdfe9f2debc55d1df16f422
Sabbirdiu <sabbir.s.dk7@gmail.com>
2020-11-24 17:59:12 +0600

    added like views

--- posts/views.py
+++ posts/views.py
@@ -5,11 +5,6 @@ from django.shortcuts import render
 def post_comment_create_and_list_view(request):
     qs = Post.objects.all()
     profile = Profile.objects.get(user=request.user)
-
-  
-
-
-   
     context = {
         'qs': qs,
         'profile': profile,
@@ -17,3 +12,32 @@ def post_comment_create_and_list_view(request):
     }
 
     return render(request, 'posts/main.html', context)
+
+@login_required
+def like_unlike_post(request):
+    user = request.user
+    if request.method == 'POST':
+        post_id = request.POST.get('post_id')
+        post_obj = Post.objects.get(id=post_id)
+        profile = Profile.objects.get(user=user)
+
+        if profile in post_obj.liked.all():
+            post_obj.liked.remove(profile)
+        else:
+            post_obj.liked.add(profile)
+
+        like, created = Like.objects.get_or_create(user=profile, post_id=post_id)
+
+        if not created:
+            if like.value=='Like':
+                like.value='Unlike'
+            else:
+                like.value='Like'
+        else:
+            like.value='Like'
+
+            post_obj.save()
+            like.save()
+
+       
+    return redirect('main-post-view')    
