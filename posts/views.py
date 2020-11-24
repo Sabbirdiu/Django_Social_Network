09e793efc7048455585efc594fee6a6fa6cbc847
Sabbirdiu <sabbir.s.dk7@gmail.com>
2020-11-24 17:56:15 +0600

    added list views

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+@login_required
+def post_comment_create_and_list_view(request):
+    qs = Post.objects.all()
+    profile = Profile.objects.get(user=request.user)
+
+  
+
+
+   
+    context = {
+        'qs': qs,
+        'profile': profile,
+       
+    }
+
+    return render(request, 'posts/main.html', context)
